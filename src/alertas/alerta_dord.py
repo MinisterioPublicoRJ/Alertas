dfb7a1aff81ae3be7af7b910d65a14d6a232dcde
ebarbara <estevan.barbara@gmail.com>
2019-05-17 16:38:56 -0300

    Alerta DORD funcional

--- src/alertas/alerta_dord.py
+++ src/alertas/alerta_dord.py
@@ -9,7 +9,7 @@ columns = [
     col('docu_nr_externo').alias('alrt_docu_nr_externo'), 
     col('docu_tx_etiqueta').alias('alrt_docu_etiqueta'), 
     col('cldc_ds_classe').alias('alrt_docu_classe'),
-    col('docu_dt_cadastro').alias('alrt_docu_date'),  
+    col('pcao_dt_andamento').alias('alrt_docu_date'),  
     col('docu_orgi_orga_dk_responsavel').alias('alrt_orgi_orga_dk'),
     col('cldc_ds_hierarquia').alias('alrt_classe_hierarquia')
 ]
@@ -17,26 +17,22 @@ columns = [
 def alerta_dord():
     documento = spark.table('exadata.mcpr_documento')
     classe = spark.table('exadata_aux.mmps_classe_hierarquia')
-    vista = spark.table('exadata_aux.mcpr_vista')
-    andamento = spark.table('exadata_aux.mcpr_andamento')
+    vista = spark.table('exadata.mcpr_vista')
+    andamento = spark.table('exadata.mcpr_andamento').filter('pcao_tpsa_dk = 2')
+   
     doc_classe = documento.join(classe, documento.docu_cldc_dk == classe.CLDC_DK, 'left')
-    doc_vista = doc_classe.join(vista, doc_classe.docu_dk == vista.vist_docu_dk, 'inner')
-    doc_andamento = doc_vista.join(andamento, doc_vista.vist_dk == andamento.pcao_vist_dk, 'inner')
+    doc_vista = doc_classe.join(vista, vista.vist_docu_dk == documento.docu_dk)
+    doc_andamento = doc_vista.join(andamento, doc_vista.vist_dk == andamento.pcao_vist_dk)
+    last_andamento = doc_andamento.select(['docu_dk', 'pcao_dt_andamento']).groupBy('docu_dk')\
+        .agg({'pcao_dt_andamento': 'max'}).withColumnRenamed('max(pcao_dt_andamento)', 'last_date')\
+        .withColumnRenamed('docu_dk', 'land_docu_dk')
+    check_andamento = doc_andamento.join(
+        last_andamento, 
+        (doc_andamento.docu_dk == last_andamento.land_docu_dk) & (doc_andamento.pcao_dt_andamento == last_andamento.last_date)
+    )
 
-    # TODO Implementar o trecho
-    # and a.pcao_dk = (                                 
-	#		SELECT MAX(z.pcao_dk)                         
-	#		FROM                                                    
-	#			mcpr_vista x                                        
-	#			JOIN mcpr_andamento z ON x.vist_dk = z.pcao_vist_dk
-	#			JOIN orgi_orgao ok ON x.VIST_ORGI_ORGA_DK = ok.ORGI_CDORGAO
-	#		WHERE 
-	#			x.vist_docu_dk = d.docu_dk                        
-	#			AND o.ORGI_TPOR_DK = 1
-	#	)
-    return doc_andamento.\
+    return check_andamento.\
         filter('docu_tpst_dk != 11').\
         filter('docu_fsdc_dk = 1').\
         filter('vist_orgi_orga_dk != docu_orgi_orga_dk_responsavel').\
-        filter('pcao_tpsa_dk = 2').\
         select(columns)
