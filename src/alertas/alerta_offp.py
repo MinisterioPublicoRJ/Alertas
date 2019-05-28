914aa9c64a7663c0414e329de97af5b24cfd6569
ebarbara <estevan.barbara@gmail.com>
2019-05-28 15:28:34 -0300

    Revisão na regra do alerta

--- src/alertas/alerta_offp.py
+++ src/alertas/alerta_offp.py
@@ -21,19 +21,21 @@ def alerta_offp():
         filter('docu_tpst_dk != 11').\
         filter('docu_fsdc_dk = 1')
     classe = spark.table('exadata_aux.mmps_classe_hierarquia')
+    apenso = spark.table('exadata.mcpr_correlacionamento').\
+        filter('corr_tpco_dk in (2, 6)')
     vista = spark.table('exadata.mcpr_vista')
     andamento = spark.table('exadata.mcpr_andamento')
     sub_andamento = spark.table('exadata.mcpr_sub_andamento').\
-        filter('stao_tppr_dk = 6497').\
-        filter('stao_nr_dias_prazo > 0').\
-        filter('stao_in_atendido is null OR stao_in_atendido = "N"')
+        filter('stao_tppr_dk = 6497')
    
-    doc_classe = documento.join(classe, documento.docu_cldc_dk == classe.CLDC_DK, 'left')
+    doc_apenso = documento.join(apenso, documento.docu_dk == apenso.CORR_DOCU_DK2, 'left').\
+        filter('corr_tpco_dk is null')
+    doc_classe = documento.join(classe, doc_apenso.docu_cldc_dk == classe.CLDC_DK, 'left')
     doc_vista = doc_classe.join(vista, doc_classe.docu_dk == vista.vist_docu_dk, 'left')
     doc_andamento = doc_vista.join(andamento, doc_vista.vist_dk == andamento.pcao_vist_dk, 'left')
     doc_sub_andamento = doc_andamento.join(sub_andamento, doc_andamento.pcao_dk == sub_andamento.stao_pcao_dk, 'left').\
         filter('stao_dk is not null').\
-        withColumn('dt_fim_prazo', expr('date_add(pcao_dt_andamento, stao_nr_dias_prazo)')).\
+        withColumn('dt_fim_prazo', expr('date_add(pcao_dt_andamento, 365)')).\
         withColumn('elapsed', lit(datediff(current_date(), 'dt_fim_prazo')).cast(IntegerType()))
 
     resultado = doc_sub_andamento.filter('elapsed > 0')
