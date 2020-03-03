676a68ba3b79ff0ce0e8afb4738585359ab63098
matteushc <matteuscunhas@gmail.com>
2020-03-03 11:42:36 -0300

    Alteração do script para execução em cluster e das variaveis enviadas do NIFI para o codigo

--- alertas.sh
+++ alertas.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export HADOOP_USER_NAME=mpmapas
 export PYTHONIOENCODING=utf8
-spark2-submit \
+spark2-submit --master yarn --deploy-mode cluster \
     --queue root.mpmapas \
     --num-executors 10 \
     --executor-cores 1 \
@@ -9,4 +9,4 @@ spark2-submit \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
-    --py-files src/alertas/*.py,packages/*.egg,packages/*.whl src/alertas/main.py
+    --py-files src/alertas/*.py,packages/*.egg,packages/*.whl src/alertas/main.py -e $SCHEMA_EXADATA -a $SCHEMA_EXADATA_AUX
