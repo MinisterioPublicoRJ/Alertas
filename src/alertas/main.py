ebf3386fe9ea02160c5976ea25b485e922cee70c
matteushc <matteuscunhas@gmail.com>
2020-03-03 11:50:27 -0300

    Inclusao do argparse para passar as variaveis

--- src/alertas/main.py
+++ src/alertas/main.py
@@ -1,6 +1,15 @@
 #-*-coding:utf-8-*-
 import base
+import argparse
 from jobs import AlertaSession
 
-session = AlertaSession()
-session.generateAlertas()
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Execute process")
+    parser.add_argument('-e','--schemaExadata', metavar='schemaExadata', type=str, help='')
+    parser.add_argument('-a','--schemaExadataAux', metavar='schemaExadataAux', type=str, help='')
+    args = parser.parse_args()
+
+    options = {'schema_exadata': args.schemaExadata, 'schema_exadata_aux' : args.schemaExadataAux}
+    session = AlertaSession(options)
+    session.generateAlertas()
