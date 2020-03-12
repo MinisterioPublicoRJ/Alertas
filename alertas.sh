a659bc2e0842bce8158fb26456a955552c8c72f7
matteushc <matteuscunhas@gmail.com>
2020-03-12 17:35:30 -0300

    Alterando os tipos de argumentos para uma lista

--- alertas.sh
+++ alertas.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export HADOOP_USER_NAME=mpmapas
 export PYTHONIOENCODING=utf8
 spark2-submit --master yarn --deploy-mode cluster \
     --queue root.mpmapas \
@@ -9,4 +8,4 @@ spark2-submit --master yarn --deploy-mode cluster \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
-    --py-files src/alertas/*.py,packages/*.egg,packages/*.whl src/alertas/main.py -e $SCHEMA_EXADATA -a $SCHEMA_EXADATA_AUX -i $IMPALA_HOST -o $IMPALA_PORT
+    --py-files src/alertas/*.py,packages/*.egg,packages/*.whl,packages/*.zip src/alertas/main.py $@
