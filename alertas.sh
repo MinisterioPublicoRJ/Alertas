08646fd95d49d64f5ab13a3232b0ce2951dd9c51
matteushc <matteuscunhas@gmail.com>
2020-03-17 16:30:06 -0300

    Adicionando parametros para otimizar a execução do processo

--- alertas.sh
+++ alertas.sh
@@ -2,10 +2,14 @@
 export PYTHONIOENCODING=utf8
 spark2-submit --master yarn --deploy-mode cluster \
     --queue root.mpmapas \
-    --num-executors 10 \
-    --executor-cores 1 \
-    --executor-memory 8g \
+    --num-executors 50 \
+    --driver-memory 6g \
+    --executor-cores 7 \
+    --executor-memory 50g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
+    --conf spark.driver.maxResultSize=6000 \
+    --conf spark.default.parallelism=100 \
+    --conf spark.sql.shuffle.partitions=100 \
     --py-files src/alertas/*.py,packages/*.egg,packages/*.whl,packages/*.zip src/alertas/main.py $@
