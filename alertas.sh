31398150e267e2d2af8c42eb1b8ba443ead7a626
matteushc <matteuscunhas@gmail.com>
2020-04-30 18:59:09 -0300

    Ajustando parametros spark

--- alertas.sh
+++ alertas.sh
@@ -2,14 +2,14 @@
 export PYTHONIOENCODING=utf8
 spark2-submit --master yarn --deploy-mode cluster \
     --queue root.mpmapas \
-    --num-executors 5 \
-    --driver-memory 6g \
+    --num-executors 10 \
+    --driver-memory 4g \
     --executor-cores 7 \
     --executor-memory 10g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
     --conf spark.driver.maxResultSize=6000 \
-    --conf spark.default.parallelism=100 \
-    --conf spark.sql.shuffle.partitions=100 \
+    --conf spark.default.parallelism=30 \
+    --conf spark.sql.shuffle.partitions=30 \
     --py-files src/alertas/*.py,packages/*.egg,packages/*.whl,packages/*.zip src/alertas/main.py $@
