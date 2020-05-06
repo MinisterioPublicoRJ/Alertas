59031af413b754efa58c8941a4f5e10a41e0f552
matteushc <matteuscunhas@gmail.com>
2020-05-06 11:17:29 -0300

    Ajustando parametros do spark

--- alertas.sh
+++ alertas.sh
@@ -4,13 +4,13 @@ spark2-submit --master yarn --deploy-mode cluster \
     --queue root.mpmapas \
     --num-executors 10 \
     --driver-memory 4g \
-    --executor-cores 7 \
-    --executor-memory 10g \
+    --executor-cores 5 \
+    --executor-memory 8g \
     --conf spark.debug.maxToStringFields=2000 \
     --conf spark.executor.memoryOverhead=4096 \
     --conf spark.network.timeout=300 \
     --conf spark.driver.maxResultSize=6000 \
     --conf spark.default.parallelism=30 \
     --conf spark.sql.shuffle.partitions=30 \
-    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35" \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M" \
     --py-files src/alertas/*.py,packages/*.egg,packages/*.whl,packages/*.zip src/alertas/main.py $@
