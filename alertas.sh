f60aca2534a4474d681df71b28adcedd29c98f9a
matteushc <matteuscunhas@gmail.com>
2020-05-08 14:08:12 -0300

    Ajustando parametros do spark

--- alertas.sh
+++ alertas.sh
@@ -7,11 +7,13 @@ spark2-submit --master yarn --deploy-mode cluster \
     --executor-cores 5 \
     --executor-memory 8g \
     --conf spark.debug.maxToStringFields=2000 \
-    --conf spark.executor.memoryOverhead=4096 \
+    --conf spark.executor.memoryOverhead=2g \
     --conf spark.driver.maxResultSize=6000 \
-    --conf spark.network.timeout=300 \
+    --conf spark.network.timeout=360 \
     --conf spark.speculation=true \
     --conf spark.speculation.quantile=0.5 \
+    --conf spark.shuffle.io.maxRetries=5 \
+    --conf spark.shuffle.io.retryWait=15s \
     --conf spark.default.parallelism=30 \
     --conf spark.sql.shuffle.partitions=30 \
     --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M" \
