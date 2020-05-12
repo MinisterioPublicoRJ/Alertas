35235d8096a991a76b9c7531a0f37af17ef62c22
matteushc <matteuscunhas@gmail.com>
2020-05-12 13:32:33 -0300

    Ajustando parametros do spark

--- alertas.sh
+++ alertas.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 export PYTHONIOENCODING=utf8
 spark2-submit --master yarn --deploy-mode cluster \
-    --queue root.mpmapas \
-    --num-executors 10 \
-    --driver-memory 4g \
+    --queue root.alertas \
+    --num-executors 12 \
+    --driver-memory 6g \
     --executor-cores 5 \
-    --executor-memory 8g \
+    --executor-memory 10g \
     --conf spark.debug.maxToStringFields=2000 \
-    --conf spark.executor.memoryOverhead=2g \
-    --conf spark.driver.maxResultSize=6000 \
+    --conf spark.executor.memoryOverhead=4g \
+    --conf spark.driver.memoryOverhead=2g \
+    --conf spark.driver.maxResultSize=5g \
     --conf spark.network.timeout=360 \
     --conf spark.speculation=true \
-    --conf spark.speculation.quantile=0.5 \
-    --conf spark.shuffle.io.maxRetries=5 \
-    --conf spark.shuffle.io.retryWait=15s \
     --conf spark.default.parallelism=30 \
     --conf spark.sql.shuffle.partitions=30 \
-    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M" \
+    --conf spark.locality.wait=0 \
+    --conf spark.shuffle.io.numConnectionsPerPeer=3 \
+    --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35" \
     --py-files src/alertas/*.py,packages/*.egg,packages/*.whl,packages/*.zip src/alertas/main.py $@
