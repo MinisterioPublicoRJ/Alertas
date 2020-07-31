2b765dbd6281038e6ddc489b9cfc53af76cc84c7
Estevan Barbará <estevan.barbara@gmail.com>
2020-07-31 14:38:02 -0300

    Inicialização alerta novo

---
+++ src/alertas/alerta_prcr.py
@@ -0,0 +1,22 @@
+#-*-coding:utf-8-*-
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import *
+
+from base import spark
+
+
+columns = [
+    col('docu_dk').alias('alrt_docu_dk'), 
+    col('docu_nr_mp').alias('alrt_docu_nr_mp'), 
+    col('docu_nr_externo').alias('alrt_docu_nr_externo'), 
+    col('docu_tx_etiqueta').alias('alrt_docu_etiqueta'), 
+    col('cldc_ds_classe').alias('alrt_docu_classe'),
+    col('docu_dt_cadastro').alias('alrt_docu_date'),  
+    col('docu_orgi_orga_dk_responsavel').alias('alrt_orgi_orga_dk'),
+    col('cldc_ds_hierarquia').alias('alrt_classe_hierarquia'),
+    col('elapsed').alias('alrt_dias_passados')
+]
+
+
+def alerta_prcr(options):
+    pass
