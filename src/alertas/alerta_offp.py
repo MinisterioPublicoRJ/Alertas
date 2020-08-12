a1f9464951b38fc0c2429605faf2066ee074b247
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-08-12 18:07:57 -0300

    OFFP contabilizava andamentos cancelados, e usava LEFT JOIN desnecessariamente, alem de contar varias vezes o mesmo documento caso ele tivesse varios oficios

--- src/alertas/alerta_offp.py
+++ src/alertas/alerta_offp.py
@@ -26,18 +26,21 @@ def alerta_offp(options):
     classe = spark.table('%s.mmps_classe_hierarquia' % options['schema_exadata_aux'])
     # vista = spark.table('%s.mcpr_vista' % options['schema_exadata'])
     vista = spark.sql("from vista")
-    andamento = spark.table('%s.mcpr_andamento' % options['schema_exadata'])
+    andamento = spark.table('%s.mcpr_andamento' % options['schema_exadata']).\
+        filter('pcao_dt_cancelamento IS NULL')
     sub_andamento = spark.table('%s.mcpr_sub_andamento' % options['schema_exadata']).\
         filter('stao_tppr_dk = 6497')
    
     doc_classe = documento.join(broadcast(classe), documento.DOCU_CLDC_DK == classe.cldc_dk, 'left')
-    doc_vista = doc_classe.join(vista, doc_classe.DOCU_DK == vista.VIST_DOCU_DK, 'left')
-    doc_andamento = doc_vista.join(andamento, doc_vista.VIST_DK == andamento.PCAO_VIST_DK, 'left')
-    doc_sub_andamento = doc_andamento.join(sub_andamento, doc_andamento.PCAO_DK == sub_andamento.STAO_PCAO_DK, 'left').\
-        filter('stao_dk is not null').\
+    doc_vista = doc_classe.join(vista, doc_classe.DOCU_DK == vista.VIST_DOCU_DK, 'inner')
+    doc_andamento = doc_vista.join(andamento, doc_vista.VIST_DK == andamento.PCAO_VIST_DK, 'inner')
+    doc_sub_andamento = doc_andamento.join(sub_andamento, doc_andamento.PCAO_DK == sub_andamento.STAO_PCAO_DK, 'inner').\
         withColumn('dt_fim_prazo', expr("to_timestamp(date_add(pcao_dt_andamento, 365), 'yyyy-MM-dd HH:mm:ss')")).\
         withColumn('elapsed', lit(datediff(current_date(), 'dt_fim_prazo')).cast(IntegerType()))
 
-    resultado = doc_sub_andamento.filter('elapsed > 0')
+    # Pode ter mais de um andamento de oficio
+    resultado = doc_sub_andamento.filter('elapsed > 0').\
+        groupBy(columns[:-1]).agg({'elapsed': 'max'}).\
+        withColumnRenamed('max(elapsed)', 'alrt_dias_passados')
     
-    return resultado.select(columns)
+    return resultado
