bc8df403af809622d93fa47ff12870b2ba5b015c
Estevan Barbará <estevan.barbara@gmail.com>
2020-09-15 23:06:02 -0300

    Alteracoes na query GATE

--- src/alertas/alerta_gate.py
+++ src/alertas/alerta_gate.py
@@ -6,6 +6,7 @@ from base import spark
 
 
 columns = [
+    col('itcn_dk').alias('alrt_dk'),
     col('docu_dk').alias('alrt_docu_dk'), 
     col('docu_nr_mp').alias('alrt_docu_nr_mp'), 
     col('docu_nr_externo').alias('alrt_docu_nr_externo'), 
@@ -16,12 +17,6 @@ columns = [
     col('cldc_ds_hierarquia').alias('alrt_classe_hierarquia'),
     col('elapsed').alias('alrt_dias_passados'),
 ]
-
-pre_columns = [
-    'docu_dk', 'docu_nr_mp', 'docu_nr_externo', 'docu_tx_etiqueta', 
-    'cldc_ds_classe', 'docu_orgi_orga_dk_responsavel', 'cldc_ds_hierarquia'
-]
-
 
 
 def alerta_gate(options):
@@ -37,11 +32,6 @@ def alerta_gate(options):
 
     doc_sem_vista = doc_vista.filter('DT_MAX_VISTA is null')
     doc_vista_anterior = doc_vista.filter('ITCN_DT_CADASTRO > DT_MAX_VISTA')
-    doc_inst_recente = doc_sem_vista.union(doc_vista_anterior)
-
-    # Caso haja mais de uma instrução maior que a data de vísta máxima, pega a que foi ativada primeiro
-    resultado = doc_inst_recente.groupBy(pre_columns).agg({'ITCN_DT_CADASTRO': 'min'}).\
-        withColumnRenamed('min(ITCN_DT_CADASTRO)', 'dt_it').\
-        withColumn('elapsed', lit(datediff(current_date(), 'dt_it')).cast(IntegerType()))
+    resultado = doc_sem_vista.union(doc_vista_anterior)
 
     return resultado.select(columns)
