686c8ee0751b5da355d1d1e573aef14c871f5b1b
rhenanbartels <rhenan.bartels@gmail.com>
2020-10-16 15:30:26 -0300

    WIP: alerta-ro

---
+++ src/alertas/alerta_ro.py
@@ -0,0 +1,36 @@
+#-*-coding:utf-8-*-
+from pyspark.sql.functions import *
+
+from base import spark
+
+
+columns = [
+    col('numero_delegacia').alias('alrt_dk'),
+    col('pip_codigo').alias('alrt_orgi_orga_dk'),
+    col('qtd_falta').alias('alrt_dias_passados')
+]
+
+
+def alerta_ro(options):
+    df = spark.sql("""
+    WITH ros_que_faltam AS (
+            SELECT
+                CAST(substring(proc_numero, 0, 3) AS INTEGER) as numero_delegacia,
+                MIN(proc_numero) proc_delegacia_inicial,
+                MAX(proc_numero) proc_delegacia_final,
+                substring(MIN(proc_numero), 5, 5) min_serial_delegacia,
+                substring(MAX(proc_numero), 5, 5) max_serial_delegacia,
+                CAST(substring(MAX(proc_numero), 5, 5) AS INTEGER) qtd_esperada,
+                CAST(substring(MAX(proc_numero), 5, 5) AS INTEGER)
+                    - COUNT(DISTINCT proc_numero) qtd_falta,
+                COUNT(DISTINCT proc_numero) total_de_ros_recebidos
+            FROM opengeo.seg_pub_in_pol_procedimento
+            WHERE year(proc_data) = year(now())
+            GROUP BY numero_delegacia
+    )
+    SELECT * FROM ros_que_faltam rqf
+    JOIN exadata_aux_dev.tb_pip_cisp tpc ON rqf.numero_delegacia = tpc.cisp_codigo
+    WHERE rqf.qtd_falta >= 1
+    """)
+
+    return df.select(columns)
