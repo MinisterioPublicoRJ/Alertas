458c4cbaf8cb890818028c6e8fb32cfa4ec6887c
Estevan Barbará <estevan.barbara@gmail.com>
2020-12-15 16:18:32 -0300

    WIP - revisão alerta

--- src/alertas/alerta_bdpa.py
+++ src/alertas/alerta_bdpa.py
@@ -9,11 +9,11 @@ mov_columns = [
     col('docu_dk'),
     col('docu_nr_mp'),
     col('docu_nr_externo'),
-    col('docu_tx_etiqueta'),
+    col('docu_tx_etiqueta')
     col('cldc_ds_classe'),
     col('cldc_ds_hierarquia'),
     col('docu_orgi_orga_dk_responsavel'),
-    col('movi_orga_dk_destino'),
+    col('dt_guia_pre'),
 ]
 
 columns = [
@@ -28,36 +28,101 @@ columns = [
     col('elapsed').alias('alrt_dias_passados'),
 ]
 
+full_columns = [
+    col('docu_dk').alias('alrt_docu_dk'), 
+    col('docu_nr_mp').alias('alrt_docu_nr_mp'), 
+    col('docu_nr_externo').alias('alrt_docu_nr_externo'), 
+    col('docu_tx_etiqueta').alias('alrt_docu_etiqueta'), 
+    col('cldc_ds_classe').alias('alrt_docu_classe'),
+    col('dt_fim_prazo').alias('alrt_docu_date'),  
+    col('docu_orgi_orga_dk_responsavel').alias('alrt_orgi_orga_dk'),
+    col('cldc_ds_hierarquia').alias('alrt_classe_hierarquia'),
+    col('elapsed').alias('alrt_dias_passados'),
+    col('orge_orga_dk').alias('alrt_orgao_baixa_dk'),
+    col('orge_nm_orgao').alias('alrt_orgao_baixa_nm'),
+]
+
 def alerta_bdpa(options):
     documento = spark.sql("from documento").\
         filter('docu_tpst_dk = 3').\
-        filter('docu_fsdc_dk = 1')
-    classe = spark.table('%s.mmps_classe_hierarquia' % options['schema_exadata_aux'])
-    item_mov = spark.table('%s.mcpr_item_movimentacao' % options['schema_exadata'])
-    movimentacao = spark.table('%s.mcpr_movimentacao'  % options['schema_exadata'])
-    vista = spark.sql("from vista")
-    andamento = spark.table('%s.mcpr_andamento' % options['schema_exadata'])
-    sub_andamento = spark.table('%s.mcpr_sub_andamento' % options['schema_exadata'])
-    tp_baixa = spark.table('%s.mmps_tp_andamento' % options['schema_exadata_aux']).\
-        filter('id_pai in (6363, 6519)')
-    orga_policia = spark.table('exadata.mprj_orgao_ext').\
-        filter('orge_tpoe_dk IN (54, 60, 61, 68)')
-
-    mov_cop = movimentacao.join(orga_policia, movimentacao.MOVI_ORGA_DK_DESTINO == orga_policia.ORGE_ORGA_DK)
-    doc_classe = documento.join(broadcast(classe), documento.DOCU_CLDC_DK == classe.cldc_dk, 'left')
-    doc_item = doc_classe.join(item_mov, documento.DOCU_DK == item_mov.ITEM_DOCU_DK, 'inner')
-    doc_mov = doc_item.join(mov_cop, doc_item.ITEM_MOVI_DK == mov_cop.MOVI_DK, 'inner').\
+        filter('docu_fsdc_dk = 1').\
+        select([
+            col('docu_dk'),
+            col('docu_nr_mp'),
+            col('docu_nr_externo'),
+            col('docu_tx_etiqueta'),
+            col('docu_orgi_orga_dk_responsavel'),
+            col('docu_cldc_dk'),
+            col('docu_orge_orga_dk_deleg_origem'),
+            col('docu_orge_orga_dk_deleg_fato'),
+            col('docu_orga_dk_origem'),
+            col('docu_dt_cadastro'),
+        ])
+    classe = spark.table('%s.mmps_classe_hierarquia' % options['schema_exadata_aux']).\
+        select([
+            col('cldc_dk'),
+            col('cldc_ds_classe'),
+            col('cldc_ds_hierarquia'),
+        ])
+    item_mov = spark.table('%s.mcpr_item_movimentacao' % options['schema_exadata']).\
+        select([
+            col('item_docu_dk'),
+            col('item_movi_dk'),
+        ])
+    movimentacao = spark.table('%s.mcpr_movimentacao'  % options['schema_exadata']).\
         withColumn(
-            'dt_guia_pre',
+            'movi_dt_guia',
             coalesce(
                 col('movi_dt_recebimento_guia'),
                 col('movi_dt_envio_guia'),
                 col('movi_dt_criacao_guia')
             )
         ).\
-        groupBy(mov_columns).agg({'dt_guia_pre': 'max'}).\
-        withColumnRenamed('max(dt_guia_pre)', 'dt_guia')
+        select([
+            col('movi_dk'),
+            col('movi_dt_guia'),
+            col('movi_orga_dk_destino'),
+        ])
+    vista = spark.sql("from vista").\
+        select([
+            col('vist_dk'),
+            col('vist_docu_dk'),
+        ])
+    andamento = spark.table('%s.mcpr_andamento' % options['schema_exadata']).\
+        select([
+            col('pcao_dk'),
+            col('pcao_vist_dk'),
+            col('pcao_dt_andamento'),
+        ])
+    sub_andamento = spark.table('%s.mcpr_sub_andamento' % options['schema_exadata']).\
+        select([
+            
+        ])
+    tp_baixa = spark.table('%s.mmps_tp_andamento' % options['schema_exadata_aux']).\
+        filter('id_pai in (6363, 6519)')
+    orga_policia = spark.table('exadata.mprj_orgao_ext').\
+        filter('orge_tpoe_dk IN (60, 61, 68)') # ORGAOS DA POLICIA
+    orga_delegacia = spark.table('exadata.mprj_orgao_ext').\
+        filter('orge_tpoe_dk IN (60, 61)') # APENAS DELEGACIAS
     
+    doc_classe = documento.join(broadcast(classe), documento.DOCU_CLDC_DK == classe.cldc_dk, 'left')
+    doc_item = doc_classe.join(item_mov, documento.DOCU_DK == item_mov.ITEM_DOCU_DK, 'inner') 
+    doc_mov = doc_item.join(movimentacao, doc_item.ITEM_MOVI_DK == movimentacao.MOVI_DK, 'inner').\
+        select(mov_columns + [col('movi_orga_dk_destino'), ])
+    last_mov = doc_mov.groupBy(['docu_dk']).agg({'dt_guia_pre': 'max'}).\
+        withColumnRenamed('max(dt_guia_pre)', 'dt_guia').\
+        withColumnRenamed('docu_dk', 'last_docu_dk')
+    
+    doc_mov_dest = last_mov.\
+        join(
+            doc_mov, 
+            [
+                doc_mov.docu_dk == last_mov.last_docu_dk,
+                doc_mov.dt_guia_pre == last_mov.dt_guia,
+            ],
+            'inner')
+    doc_mov_cop = doc_mov_dest.join(orga_policia, doc_mov_dest.movi_orga_dk_destino == orga_policia.ORGE_ORGA_DK)
+
     doc_vista = documento.join(vista, doc_classe.DOCU_DK == vista.VIST_DOCU_DK, 'inner')
     doc_andamento = doc_vista.join(andamento, doc_vista.VIST_DK == andamento.PCAO_VIST_DK, 'inner')
     last_andamento = doc_andamento.\
@@ -76,7 +141,7 @@ def alerta_bdpa(options):
             last_andamento,
             [
                 doc_baixa.DOCU_DK == last_andamento.last_docu_dk,
-                doc_baixa.PCAO_DT_ANDAMENTO == last_andamento.dt_last_andamento
+                doc_baixa.PCAO_DT_ANDAMENTO == last_andamento.dt_last_andamento,
             ],
             'inner'
         ).\
@@ -85,8 +150,22 @@ def alerta_bdpa(options):
             col('stao_nr_dias_prazo'),
         ])
 
-    doc_lost = doc_mov.join(last_baixa, doc_mov.docu_dk == last_baixa.last_docu_dk, 'inner').\
+    doc_lost = doc_mov_cop.join(last_baixa, doc_mov_cop.docu_dk == last_baixa.last_docu_dk, 'inner').\
         withColumn("dt_fim_prazo", expr("date_add(dt_guia, stao_nr_dias_prazo)")).\
-        withColumn('elapsed', lit(datediff(current_date(), 'dt_fim_prazo')).cast(IntegerType()))
+        withColumn('elapsed', lit(datediff(current_date(), 'dt_fim_prazo')).cast(IntegerType())).\
+        select(full_columns)
+    
+    dp_mov = doc_mov.join(orga_policia, doc_mov.movi_orga_dk_destino == orga_policia.ORGE_ORGA_DK)
+    last_dp_mov = dp_mov.\
+        groupBy([
+            'docu_dk',
+            'orge_orga_dk',
+            'orge_nm_orgao',
+        ]).agg({'dt_guia_pre': 'max'}).\
+        withColumnRenamed('max(dt_guia_pre)', 'dt_guia_dp').\
+        withColumnRenamed('docu_dk', 'dp_docu_dk').\
+        withColumnRenamed('orge_orga_dk', 'dp_resp_dk').\
+        withColumnRenamed('orge_nm_orgao', 'dp_resp_nm')
+    doc_dp_lost = doc_lost.join(last_dp_mov, doc_lost.docu_dk == last_dp_mov.dp_docu_dk, 'left')
 
     return doc_lost.filter('elapsed > 0').select(columns)
