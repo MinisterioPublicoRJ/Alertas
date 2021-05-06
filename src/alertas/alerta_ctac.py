3bff9d2c1843f1edea26c35ca0062181dc64e5e3
bernardoccordeiro <bernardoccordeiro@gmail.com>
2021-05-06 20:13:03 -0300

    Alerta CTAC levando em conta andamentos de ciencia

--- src/alertas/alerta_ctac.py
+++ src/alertas/alerta_ctac.py
@@ -22,10 +22,18 @@ key_columns = [
 ]
 
 def alerta_ctac(options):
+    """
+    Alerta de TACs que não foram comunicadas ao CSMP. Um andamento de TAC
+    ativa o alerta. Um andamento de ciência ao CSMP em data maior ou igual ao
+    TAC desativa o alerta. Um andamento de ofício com destinatário ao CSMP
+    (vide regex) também desativa o alerta.
+    """
     ANDAMENTOS_OFICIO = (7436, 6581, 6497, 6614, 6615, 6616, 6617, 6618, 6619, 6126, 6989)
     ANDAMENTOS_TAC = (1007, 6304, 7858, 6326, 6655, 6670, 4114)
-    # 6647, 6643, 6683, 6684, 6685, 6573 Ciência ao CSMP
-    ANDAMENTOS_TOTAL = ANDAMENTOS_TAC + ANDAMENTOS_OFICIO
+    ANDAMENTOS_CIENCIA = (6647, 6643, 6683, 6684, 6685, 6573)
+
+    ANDAMENTOS_TAC_CIENCIA = ANDAMENTOS_TAC + ANDAMENTOS_CIENCIA
+    ANDAMENTOS_OFICIO_CIENCIA = ANDAMENTOS_OFICIO + ANDAMENTOS_CIENCIA
 
     REGEX_CSMP = (
         "(CSMP|CONSELHO SUPERIOR|CONSELHO SUPERIOR DO MINIST[ÉE]RIO P[ÚU]BLICO)"
@@ -39,7 +47,7 @@ def alerta_ctac(options):
             FROM 
             (
                 SELECT docu_dk, docu_nr_mp, docu_orgi_orga_dk_responsavel,
-                CASE WHEN stao_tppr_dk IN {ANDAMENTOS_OFICIO} THEN pcao_dt_andamento ELSE NULL END as dt_oficio_csmp,
+                CASE WHEN stao_tppr_dk IN {ANDAMENTOS_OFICIO_CIENCIA} THEN pcao_dt_andamento ELSE NULL END as dt_oficio_csmp,
                 CASE WHEN stao_tppr_dk IN {ANDAMENTOS_TAC} THEN pcao_dt_andamento ELSE NULL END as dt_tac
                 FROM documento
                 JOIN (
@@ -49,7 +57,11 @@ def alerta_ctac(options):
                     JOIN {schema_exadata}.mcpr_sub_andamento ON stao_pcao_dk = pcao_dk
                     WHERE pcao_dt_cancelamento IS NULL
                     AND pcao_dt_andamento >= to_timestamp('{date_tac_begin}', 'yyyy-MM-dd')
-                    AND (stao_tppr_dk in {ANDAMENTOS_TAC} OR (stao_tppr_dk in {ANDAMENTOS_OFICIO} AND upper(stao_destinatario) REGEXP '{REGEX_CSMP}'))
+                    AND (
+                        stao_tppr_dk in {ANDAMENTOS_TAC_CIENCIA} OR
+                        (stao_tppr_dk in {ANDAMENTOS_OFICIO} AND upper(stao_destinatario) REGEXP '{REGEX_CSMP}')
+
+                    )
                 ) T ON T.vist_docu_dk = docu_dk
             ) A
             GROUP BY docu_dk, docu_nr_mp, docu_orgi_orga_dk_responsavel
@@ -61,7 +73,8 @@ def alerta_ctac(options):
             date_tac_begin=options['date_tac_begin'],
             ANDAMENTOS_TAC=ANDAMENTOS_TAC,
             ANDAMENTOS_OFICIO=ANDAMENTOS_OFICIO,
-            ANDAMENTOS_TOTAL=ANDAMENTOS_TOTAL,
+            ANDAMENTOS_TAC_CIENCIA=ANDAMENTOS_TAC_CIENCIA,
+            ANDAMENTOS_OFICIO_CIENCIA=ANDAMENTOS_OFICIO_CIENCIA,
             REGEX_CSMP=REGEX_CSMP
         )
     )
